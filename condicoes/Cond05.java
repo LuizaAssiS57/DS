e0a065a8a855d822f73e1f49a7a429cec9a1e412
Luiza Assis <luizaassis57@gmail.com>
2026-08-17 14:11:52 -0300

    Add files via upload

---
+++ condicoes/Cond05.java
@@ -0,0 +1,25 @@
+package condicoes;
+
+import java.util.Scanner;
+
+public class Cond05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informar o nome: ");
+        String nome = sc.nextLine();
+        
+        System.out.println("Informar a senha: ");
+        String senha = sc.nextLine();
+
+        if (nome.equals("Raquel") && senha.equals("1234") ) {
+            System.out.println("Login realizado com sucesso!");
+        }else if (nome.equals("Maria") && senha.equals("5678")){
+            System.out.println("Login realizado com sucesso!");
+        }else{
+            System.out.println("Dados incorretos");
+        }
+
+        sc.close();
+    }
+}
