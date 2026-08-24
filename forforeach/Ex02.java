207d1a5103839ddc2ba5f1649de1e91ad170a10f
Luiza Assis <luizaassis57@gmail.com>
2026-08-24 14:31:00 -0300

    Exemplos FOR

---
+++ forforeach/Ex02.java
@@ -0,0 +1,9 @@
+package forforeach;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i+= 2) {
+            System.out.println("Os números são: "+i);
+        }
+    }
+}
