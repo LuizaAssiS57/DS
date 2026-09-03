27d090ad07efa787525646db6c306c4e04135065
Luiza Assis <luizaassis57@gmail.com>
2026-09-03 13:36:38 -0300

    Reserva hotel

--- diversao09/ReservaHotel.java
+++ diversao09/ReservaHotel.java
@@ -104,23 +104,93 @@ public class ReservaHotel {
                     }
                     break;
                 case 4:
-                    
+                    if (totalReservas == 0) {
+                        System.out.println("Nenhuma reserva foi realizada ainda!");
+                    } else {
+                        int numeroQuartoConsulta;
+                        int posicaoQuartoConsulta = -1;
+                        boolean encontrouReserva = false;
+
+                        System.out.print("Informe o número do quarto para consulta: ");
+                        numeroQuartoConsulta = sc.nextInt();
+
+                        for (int i = 0; i < quantidadeQuartos; i++) {
+                            if (numerosQuartos[i] == numeroQuartoConsulta) {
+                                posicaoQuartoConsulta = i;
+                                break;
+                            }
+                        }
+
+                        if (posicaoQuartoConsulta == -1) {
+                            System.out.println("Este quarto não existe!");
+                        } else {
+                            System.out.println("Reservas do quarto " + numeroQuartoConsulta + ":");
+                            for (int i = 0; i < totalReservas; i++) {
+                                if (quartoReserva[i] == numeroQuartoConsulta) {
+                                    System.out.println("- " + hospedes[i]);
+                                    encontrouReserva = true;
+                                }
+                            }
+
+                            if (!encontrouReserva) {
+                                System.out.println("Não há reservas para este quarto!");
+                            }
+                        }
+                    }
                     break;
                 case 5:
-                    
+                    if (totalReservas == 0) {
+                        System.out.println("Nenhuma reserva foi realizada ainda!");
+                    } else {
+                        String nomeConsulta;
+                        boolean encontrouHospede = false;
+
+                        System.out.print("Informe o nome do hóspede: ");
+                        nomeConsulta = sc.nextLine();
+
+                        System.out.println("Reservas do hóspede " + nomeConsulta + ":");
+                        for (int i = 0; i < totalReservas; i++) {
+                            if (hospedes[i].equalsIgnoreCase(nomeConsulta)) {
+                                System.out.println("- Quarto " + quartoReserva[i]);
+                                encontrouHospede = true;
+                            }
+                        }
+
+                        if (!encontrouHospede) {
+                            System.out.println("Não há reservas para este hóspede!");
+                        }
+                    }
                     break;
                 case 6:
-                    
+                    if (!quartosCadastrados) {
+                        System.out.println("Nenhum quarto foi cadastrado ainda!");
+                    } else {
+                        System.out.println("\n--- RELATÓRIO GERAL ---");
+                        for (int i = 0; i < quantidadeQuartos; i++) {
+                            int contadorReservas = 0;
+
+                            for (int j = 0; j < totalReservas; j++) {
+                                if (quartoReserva[j] == numerosQuartos[i]) {
+                                    contadorReservas++;
+                                }
+                            }
+
+                            System.out.println("Quarto: " + numerosQuartos[i]);
+                            System.out.println("Camas disponíveis: " + camasDisponiveis[i]);
+                            System.out.println("Quantidade de reservas: " + contadorReservas);
+                            System.out.println("---------------------------");
+                        }
+                    }
                     break;
                 case 7:
-                    
+                    System.out.println("Sistema encerrado!");
                     break;
             
                 default:
+                    System.out.println("Opção inválida!");
                     break;
             }
         } while (opcao != 7);
-
 
         sc.close();
     }
